8bd1f9c9cab02590c4b666bdadc944f7175a53c9
bogzbonny <bogzbonny@protonmail.com>
2026-03-30 11:21:41 -0700

    position correction

--- src/lib.rs
+++ src/lib.rs
@@ -358,6 +358,7 @@ fn buf_get_lines() -> Vec<String> {
 /// Get current buffer position
 fn get_pos() -> (usize, usize) {
     let (line, col) = Window::current().get_cursor().unwrap_or((0, 0));
+    let line = line.saturating_sub(1);
     (col, line) // (x, y) = (col, line)
 }
 
